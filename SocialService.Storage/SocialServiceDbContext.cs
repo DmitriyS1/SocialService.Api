6fb52680d260daba032273b473bc3cd657e8c07b
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2021-07-22 20:16:36 +0300

    Context сконфигурирован

--- SocialService.Storage/SocialServiceDbContext.cs
+++ SocialService.Storage/SocialServiceDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SocialService.Storage.ValueObjects;
 
 namespace SocialService.Storage
 {
     public class SocialServiceDbContext : DbContext
     {
-
+        DbSet<User> Users { get; set; }
     }
 }
---
+++ SocialService.Storage/ValueObjects/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialService.Storage.ValueObjects
+{
+    public class User
+    {
+        // Explain about immutable model
+        public User(
+            string login,
+            int popularity)
+        {
+            Login = login;
+            Popularity = popularity;
+        }
+
+        public string Login { get; }
+
+        public int Popularity { get; }
+    }
+}
