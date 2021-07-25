11d2ec695546b4649adb2cc9676484ce96057495
Dmitriy Semenov <dmitriy.a.semenov@outlook.com>
2021-07-25 16:33:06 +0300

    Добавлено ограничение на кол-во получаемых пользователей

--- SocialService.Api/Controllers/PopularityController.cs
+++ SocialService.Api/Controllers/PopularityController.cs
@@ -8,6 +8,7 @@ namespace SocialService.Api.Controllers
     [ApiController]
     public class PopularityController : ControllerBase
     {
+        private static readonly int POPULAR_USERS_MAX_COUNT = 150;
         private readonly IPopularityService _popularityService;
 
         public PopularityController(
@@ -19,9 +20,9 @@ namespace SocialService.Api.Controllers
         [HttpGet("users")]
         public async Task<IActionResult> TopPopularUsers([FromQuery] int count)
         {
-            if (count <= 0)
+            if (count <= 0 || count > POPULAR_USERS_MAX_COUNT)
             {
-                return StatusCode(422, "Count of users must be positive");
+                return StatusCode(422, $"Count of users must be positive and less than {POPULAR_USERS_MAX_COUNT}");
             }
 
             var topUsers = await _popularityService.GetTopAsync(count);
